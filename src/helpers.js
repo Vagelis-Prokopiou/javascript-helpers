0b274bdc78312c21bbc9418abb663c5d09540eef
Vagelis Prokopiou <vagelis.prokopiou@gmail.com>
2017-10-27 12:56:32 +0300

    Update helpers.js

--- src/helpers.js
+++ src/helpers.js
@@ -15,7 +15,22 @@ localStorage.setItem = function (key, obj)
 const saveObjectToLocalStorage = (key, object) => localStorage.setItem(key, JSON.stringify(object));
 const getObjectFromLocalStorage = key => JSON.parse(localStorage.getItem(key));
 
+const extractIntegersFromString = function(str)
+{
+    return str.split(" ")
+        .filter(value => !isNaN(value))
+        .map(value => parseInt(value));
+}
+const extractFloatsFromString = function(str)
+{
+    return str.split(" ")
+        .filter(value => !isNaN(value))
+        .map(value => parseFloat(value));
+}
+
 module.exports = {
-    saveObjectToLocalStorage,
-    getObjectFromLocalStorage
+    saveObjectToLocalStorage
+    , getObjectFromLocalStorage
+    , extractIntegersFromString
+    , extractFloatsFromString
 }
